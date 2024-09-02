5f91b4112c18acef241a7f47c9e571fa899fbd13
windsorrios <windsorrios27@gmail.com>
2024-09-02 19:36:00 -0400

    Contador de tiempo

--- contador.ino
+++ contador.ino
@@ -1,21 +1,35 @@
 #define PULS    2
-#define PASO     1
-unsigned int cont=0;
+#define PASO    1
+#define BAURENT   9600
+#define RESET   3
+#define VALOR   0
 
-void setup(){
+unsigned int cont;
+
+void setup()
+{
+    Serial.begin(BAURENT);
     pinMode(PULS, INPUT_PULLUP);
+    pinMode(RESET, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(PULS), interrumpir, FALLING);
+    attachInterrupt(digitalPinToInterrupt(RESET), interrumpir, FALLING);
 
 
 /* LOGICA DE UNO
 */
 }
-void loop(){
-
+void loop()
+{
+    delay(5000);
 
 }
 void interrumpir(){
     //cont=cont+1;
-    cont+=1;
-
+    //cont+=1;
+    cont+=PASO;
+    Serial.println(cont);
+    
 }
+void interrumpir()
+    cont+=PASO;
+    sERIAL.PRINTLN(cont);
